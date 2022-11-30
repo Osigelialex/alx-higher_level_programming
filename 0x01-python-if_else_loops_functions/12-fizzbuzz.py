771b842eaa43191852be5bf45cbd673dae1cd61a
Osigelialex <osigelialex@gmail.com>
2022-11-30 07:57:08 -0800

    submit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    str = ""
+    for i in range(101):
+        if i % 3 == 0:
+            str += "fizz"
+        if i % 5 == 0:
+            str += "buzz"
+        if str == "":
+            print(i)
+        print("{}".format(str))
