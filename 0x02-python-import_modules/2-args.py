a35cef0d7a4f57b297ca1ed662add17289595f41
Osigelialex <osigelialex@gmail.com>
2022-12-24 11:18:13 -0800

    submit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
+
+def print_arguments():
+    length = len(argv) - 1
+    
+    if length == 0:
+        print("{:d} arguments.".format(length))
+    if length == 1:
+        print("{:d} argument:".format(length))
+        print("{:d}: {:s}".format(length, argv[length]))
+    if length > 1:
+        print("{:d} arguments:".format(length))
+        for i in range(1, length):
             print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
 
 if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
+    from sys import argv
+    print_arguments()
