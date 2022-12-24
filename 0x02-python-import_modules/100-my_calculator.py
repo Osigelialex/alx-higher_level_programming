f0b053edffd8f00575b599f0ec874573aafca2ce
Osigelialex <osigelialex@gmail.com>
2022-12-24 13:15:01 -0800

    submit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,18 +4,18 @@ def calculate(count, a, op, b):
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif op != "+" and op != "-" and op != "*" and op != "/":
+    elif op not in "*/-+":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
         if op == "/":
-            print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         if op == "*":
-            print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         if op == "-":
-            print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
         if op == "+":
-            print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 if __name__ == "__main__":
     from calculator_1 import add, mul, div, sub
