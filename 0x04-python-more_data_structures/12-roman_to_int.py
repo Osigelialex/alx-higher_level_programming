de196b10e9afb9ac956c88546c03fa4d1123d55c
Osigelialex <osigelialex@gmail.com>
2022-12-25 08:25:29 -0800

    submit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    syms = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    syms = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     num = 0
 
-    if roman_string is None or type(roman_string) != str:
+    if roman_string is None or type(roman_string) != str or roman_string = "":
         return 0
     for i in range(len(roman_string) - 1):
         if syms[roman_string[i]] < syms[roman_string[i + 1]]:
