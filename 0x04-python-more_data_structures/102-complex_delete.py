174622a55cc87ef25fbb5601adec9f213989480b
Osigelialex <osigelialex@gmail.com>
2022-12-25 11:54:37 -0800

    submit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def complex_delete(a_dictionary, value):
-    for i in a_dictionary:
-        if a_dictionary[i] == value:
-            del a_dictionary[i]
+    for k, v in list(a_dictionary.items()):
+        if v == value:
+            del a_dictionary[k]
     return a_dictionary
