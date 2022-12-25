1cc0da9ff88bdafc609c3a156584d366254d6fa6
Osigelialex <osigelialex@gmail.com>
2022-12-25 08:09:31 -0800

    submit

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    syms = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    num = 0
+
+    for i in range(len(roman_string) - 1):
+        if syms[roman_string[i]] < syms[roman_string[i + 1]]:
+            num = num + syms[roman_string[i + 1]] - syms[roman_string[i]]
+        else:
+            num = num + syms[roman_string[i]]
+    length = len(roman_string)
+    if syms[roman_string[length - 2]] >= syms[roman_string[length - 1]]:
+        num = num + syms[roman_string[length - 1]]
+    return num
