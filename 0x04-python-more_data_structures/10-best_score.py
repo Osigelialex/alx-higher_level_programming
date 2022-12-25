b2c67e5838904150631c84183879f68a943724c4
Osigelialex <osigelialex@gmail.com>
2022-12-25 06:44:02 -0800

    submit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,9 @@ def best_score(my_dict):
     if my_dict is None:
         return None
     max_value = 0
-    name = ""
+    max_name = None
     for key, value in my_dict.items():
         if value > max_value:
             max_value = value
-            name = key
-    return name
+            max_name = key
+    return max_name
