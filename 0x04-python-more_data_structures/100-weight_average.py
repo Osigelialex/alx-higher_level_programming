cd12991b2a49ba765c70481ac624af2d467fe282
Osigelialex <osigelialex@gmail.com>
2022-12-25 11:23:53 -0800

    submit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+       return 0
+    numerator = 0
+    denominator = 0
+    average = 0
+    for i in my_list:
+        numerator += i[0] * i[1]
+        denominator += i[1]
+    average = numerator / denominator
+    return average
