94c8ff8238d2b75e85b32765b3aace0698ec30d3
Osigelialex <osigelialex@gmail.com>
2022-12-25 03:16:14 -0800

    submit

--- 0x04-python-more_data_structures/1-main.py
+++ 0x04-python-more_data_structures/1-main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-search_replace = __import__('1-search_replace').search_replace
+number_keys = __import__('5-number_keys').number_keys
 
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
+a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
+nb_keys = number_keys(a_dictionary)
+print("Number of keys: {:d}".format(nb_keys))
