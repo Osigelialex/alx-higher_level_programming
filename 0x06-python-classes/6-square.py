d4d5ec8d2caf1a7a82ed43c9f7ca09153632024a
Osigelialex <osigelialex@gmail.com>
2022-12-26 10:01:16 -0800

    submit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -16,18 +16,35 @@ class Square:
 
     """gets the value"""
 
+    @property
     def size(self):
         return self.__size
 
     """sets the size"""
 
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
+
+    """gets position value"""
+
+    @property
+    def position(self):
+        return self.__position
+
+    """sets position value"""
+
+    @position.setter
+    def position(self, value):
+        if any([i < 0 for i in value]) or len(value) < 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     """prints in stdout the square"""
 
