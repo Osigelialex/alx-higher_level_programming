512d7185415d8fdd6a9989e4b0db4f83ec36d361
Osigelialex <osigelialex@gmail.com>
2023-01-03 10:30:19 -0800

    submit

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -71,3 +71,6 @@ class Rectangle:
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
+
+    def __delete__(self):
+        print("Bye rectangle...")
