ebd0d0224f331d6d3282bccf10a4bda669d8eeac
Osigelialex <osigelialex@gmail.com>
2023-01-09 14:20:15 -0800

    submit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,4 +12,4 @@ def inherits_from(obj, a_class):
     a class, otherwise False
     """
 
-     return type(obj) != a_class and isinstance(obj, a_class)
+     return isinstance(obj, a_class) and issubclass(type(obj), a_class)
