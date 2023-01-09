1cc2ab43aa9380efbc9d0555cda3de5a28da5831
Osigelialex <osigelialex@gmail.com>
2023-01-09 13:57:14 -0800

    Added function to check kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that checks state of an object
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function returns true if object is an instance
+    of, or if the othe object is an instance of a 
+    class that inherited from, the specified class ; otherwise False.
+    """
+    return isinstance(obj, a_class)
