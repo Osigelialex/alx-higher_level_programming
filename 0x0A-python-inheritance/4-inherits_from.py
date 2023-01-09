8f88993ce4a243191b2c770428e55411cba188e9
Osigelialex <osigelialex@gmail.com>
2023-01-09 14:17:10 -0800

    big fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,4 +12,4 @@ def inherits_from(obj, a_class):
     a class, otherwise False
     """
 
-    return isinstance(obj, a_class) and issubclass(type(obj), a_class)
+     return type(obj) != a_class and isinstance(obj, a_class)
