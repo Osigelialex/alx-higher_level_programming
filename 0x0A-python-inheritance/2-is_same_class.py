223d4e856d83b7dfe5717ae009cc01445f700ccc
Osigelialex <osigelialex@gmail.com>
2023-01-09 13:46:44 -0800

    bug fixes

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that checks if object is an instance of class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function returns true if obj is instance of class
+    """
+
+    return isinstance(obj, a_class)
