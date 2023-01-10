1409940d68409d91f261febfda5b92b4a041d193
Osigelialex <osigelialex@gmail.com>
2023-01-10 10:28:24 -0800

    Added function to read files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""function to read file"""
+
+
+def read_file(filename=""):
+    """filename: name of file"""
+    with open(filename, encoding="utf-8") as a_file:
+        a_file.read()
