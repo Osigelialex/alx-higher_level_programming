c3c91e9764b5e8dc1dcd682700ceacbbaa02eb77
Osigelialex <osigelialex@gmail.com>
2023-01-10 11:05:55 -0800

    Added function to append to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""function that appends to file"""
+
+def append_write(filename="", text=""):
+    """returns the number of characters appended to file"""
+    with open(filename, "a") as f:
+        return f.append(text)
