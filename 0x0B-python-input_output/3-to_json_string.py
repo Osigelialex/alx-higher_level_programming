1236d17a0f0dc227f61d376c66d00db675bede8f
Osigelialex <osigelialex@gmail.com>
2023-01-10 11:27:20 -0800

    Added functionality

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+
+"""functin that returns the JSON represention of object"""
+
+
+def to_json_string(my_obj):
+    """module to_json_string"""
+    return json.dumps(my_obj)
