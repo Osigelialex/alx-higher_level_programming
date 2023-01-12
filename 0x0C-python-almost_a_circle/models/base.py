2c7a9dbe74fe4bca1e998b5f952e783ad2e60f76
Osigelialex <osigelialex@gmail.com>
2023-01-12 11:22:30 -0800

    Created base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Base class"""
+
+
+class Base:
+    """Initialize base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
