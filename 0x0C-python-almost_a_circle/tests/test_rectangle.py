3ed29a554788d61cba1ed6d1bb6f8f78fe39c13b
Osigelialex <osigelialex@gmail.com>
2023-01-15 10:36:08 -0800

    wrote unit test for methods and classes

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Unit test class Square"""
+import unittest
+from models.rectangle import Square
+import sys
+from io import stringIO
---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Unit test class Square"""
+import unittest
+from models.square import Square
+import sys
+from io import StringIO
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"test_square.py" [New File]                                                                   0,0-1         All
+
