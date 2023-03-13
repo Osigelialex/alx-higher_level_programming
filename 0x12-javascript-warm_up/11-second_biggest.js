b405f71a9732c7229d0b70c81b94f75fe9d622e6
Osigelialex <osigelialex@gmail.com>
2023-03-13 19:17:05 +0000

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,14 +6,17 @@ const length = process.argv.length - 2;
 if (length < 4) {
   console.log(0);
 } else {
-  let max = nums[2];
-  let second = nums[2];
+  let max = Number.NEGATIVE_INFINITY;
+  let second = Number.NEGATIVE_INFINITY;
 
   for (let i = 2; i < length; i++) {
-    if (nums[length] > max) {
-      secod = max;
+    if (nums[i] > max) {
+      second = max;
       max = nums[i];
     }
+    if (nums[i] < max && nums[i] > second) {
+      second = nums[i];
+    }
   }
-  console.log(max);
+  console.log(second);
 }
