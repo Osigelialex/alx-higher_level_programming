c9154b70e40e64b9385601f641c4f1d6efc87921
Osigelialex <osigelialex@gmail.com>
2023-03-13 16:59:07 +0000

    Fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const process = require('process');
 const value = process.argv[2];
 
 if (isNaN(value)) {
-  console.log('Missing Size');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < value; i++) {
     console.log('X'.repeat(value));
