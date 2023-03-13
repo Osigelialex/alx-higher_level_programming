66f87d6b2d7442806582af9ae091bb0d10436418
Osigelialex <osigelialex@gmail.com>
2023-03-13 18:44:25 +0000

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,16 +7,12 @@ if (length < 4) {
   console.log(0);
 } else {
   let max = nums[2];
-  let firstMax = nums[2];
+  let second = nums[2];
 
   for (let i = 2; i < length; i++) {
-    if (nums[length] > firstMax) {
-      firstMax = nums[length];
-    }
-  }
-  for (let j = 2; j < length; j++) {
-    if (nums[length] > max && nums[length] !== firstMax) {
-      max = nums[length];
+    if (nums[length] > max) {
+      secod = max;
+      max = nums[i];
     }
   }
   console.log(max);
