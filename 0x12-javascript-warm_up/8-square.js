970d97e1de174fa154bdd625eb2727dd66b3137f
Osigelialex <osigelialex@gmail.com>
2023-03-13 16:39:53 +0000

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,17 +2,19 @@
 
 const process = require('process');
 const value = process.argv[2];
-square = '';
+let square = '';
 
 if (isNaN(value)) {
   console.log('Missing Size');
 }
-if (value > 0){
+if (value > 0) {
   for (let i = 0; i < value; i++) {
     for (let j = 0; j < value; j++) {
       square += 'x';
     }
+    if (i !== value - 1) {
       square += '\n';
+    }
   }
   console.log(square);
 }
