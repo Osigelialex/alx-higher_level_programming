d416d88e2f341b441c3d6189a18963b6082ab995
Osigelialex <osigelialex@gmail.com>
2023-03-18 13:58:36 -0700

    Added file

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Displays all values in the states table of
+hbtn_0e_0_usa when name matches argument
+'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = mySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3]
+                    name=argv[4]
+    cursor = conn.Cursor()
+    query = "SELECT * FROM states WHERE name = {}".format(name)
+    cursor.execute(query)
+    [print(i) for i in cursor.fetchall()]
