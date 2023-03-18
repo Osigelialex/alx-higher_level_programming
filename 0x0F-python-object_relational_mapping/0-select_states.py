eb1d707d7a853dce25441b6fbb25415e15d0ec57
Osigelialex <osigelialex@gmail.com>
2023-03-18 12:51:01 -0700

    Added shebang

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,12 @@
-"""
-connects to mysql database
-"""
+#!/usr/bin/python3
+
+
 import MySQLdb
 from sys import argv
 
+"""
+Script to list all states
+"""
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                        passwd=argv[2], db=argv[3], charser="utf8")
