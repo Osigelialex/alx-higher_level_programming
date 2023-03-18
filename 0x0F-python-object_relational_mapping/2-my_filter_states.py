42bfeac1df6e8dff400057e0a03231ab3b262bf4
Osigelialex <osigelialex@gmail.com>
2023-03-18 14:06:56 -0700

    Fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     conn = mySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3]
                     name=argv[4]
     cursor = conn.Cursor()
-    query = "SELECT * FROM states WHERE name = {}".format(name)
+    query = "SELECT * FROM states WHERE name = {}".format(sys.argv[4])
     cursor.execute(query)
     [print(i) for i in cursor.fetchall()]
