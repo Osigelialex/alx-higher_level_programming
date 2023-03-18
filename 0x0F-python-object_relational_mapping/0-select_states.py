d4ccd9cdff2a6eb5aafed439640f949fa5f8a57e
Osigelialex <osigelialex@gmail.com>
2023-03-18 12:47:43 -0700

    Updated script to not exucute on import

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,13 +2,15 @@
 connects to mysql database
 """
 import MySQLdb
+from sys import argv
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root",
-                       passwd="root", db="hbtn_0e_0_us", charser="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                       passwd=argv[2], db=argv[3], charser="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
