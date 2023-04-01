61086928eb4738ffa5247a335bf1bbcefbbc2e12
Osigelialex <osigelialex@gmail.com>
2023-04-01 05:16:44 -0700

    Fixed bug

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
 
     print('Body response:')
     print(f"\t- type: {type(response.text)}")
-    print(f"\t- content: {response.content}")
+    print(f"\t- content: {response.content.decode('utf-8')}")
