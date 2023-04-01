8641805f437b118f682458f486701a463b97acb7
Osigelialex <osigelialex@gmail.com>
2023-04-01 05:10:51 -0700

    Task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+if __name__ == "__main__":
+    import requests
+
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+
+    print('Body response:')
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.content}")
