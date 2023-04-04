2068242d53715e9afa013a6e62f79f35cc1bb712
Osigelialex <osigelialex@gmail.com>
2023-04-04 13:11:39 -0700

    update

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,4 +10,7 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     response = requests.get(url)
-    print(response.headers['X-Request-Id'])
+
+    # check if request was successful
+    if response.status_code == 200:
+        print(response.headers['X-Request-Id'])
