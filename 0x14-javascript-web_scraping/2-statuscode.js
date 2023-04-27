784c72d1131f1f3dc4d5db1a3a0bd67ab7e04b93
Osigelialex <osigelialex@gmail.com>
2023-04-27 16:16:12 +0000

    task 2 fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 
 const PATH = process.argv[2];
 request(PATH, (error, response, body) => {
-  console.log('code: ', response && response.statusCode);
+  console.log('code:', response && response.statusCode);
 }); 
