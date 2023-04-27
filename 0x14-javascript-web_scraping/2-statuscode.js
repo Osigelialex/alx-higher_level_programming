1872416919a14241a824c7503d2d93983110a3c5
Osigelialex <osigelialex@gmail.com>
2023-04-27 16:13:53 +0000

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const PATH = process.argv[2];
+request(PATH, (error, response, body) => {
+  console.log('code: ', response && response.statusCode);
+}); 
