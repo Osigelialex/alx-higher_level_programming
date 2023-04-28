601b58002519875696aac7132f43b025cd47b2fd
Osigelialex <osigelialex@gmail.com>
2023-04-28 19:18:22 +0000

    fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -13,15 +13,19 @@ request(options, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const completed = body.filter(obj => obj.completed === true);
-    const number = {};
+    try {
+      const completed = body.filter(obj => obj.completed === true);
+      const number = {};
 
-    for (let i = 1; i <= 10; i++) {
-      const counter = completed.reduce((count, value) =>
-        (value.userId === i ? count + 1 : count), 0);
-      number[i] = counter;
-    }
+      for (let i = 1; i <= 10; i++) {
+        const counter = completed.reduce((count, value) =>
+          (value.userId === i ? count + 1 : count), 0);
+        number[i] = counter;
+      }
 
-    console.log(number);
+      console.log(number);
+    } catch(error) {
+        console.error(error);
+      }
   }
 });
